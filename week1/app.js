40abfde8b8f9c9fa7398681110baae6547546fbf
Mark Baldwin <baldwin.mv05@gmail.com>
2021-09-24 09:58:49 -0600

    Did NPM install express and made sure to be in the right file to npm start, now working

--- week1/app.js
+++ week1/app.js
@@ -1,10 +1,21 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
-const routes = require('./routes')
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
-console.log(routes.someText);
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
 
-const server = http.createServer(routes.handler);
+//const routes = require('./routes')
+
+//console.log(routes.someText);
+
+const server = http.createServer(app);
 
 server.listen(3000);
 
