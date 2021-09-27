fcd50ad0854c9b5e4a52561863b6b98280a67c83
Mark Baldwin <baldwin.mv05@gmail.com>
2021-09-26 20:25:00 -0600

    Section 6 thru handlebars

--- week1/app.js
+++ week1/app.js
@@ -1,22 +1,28 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const exp = require('constants');
 
 const app = express();
 
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'Views', '404pageNotFound.html' ));
+    res.status(404).render('404pageNotFound', {docTitle: '404 Page not Found'})
+    //res.status(404).sendFile(path.join(__dirname, 'Views', '404pageNotFound.html' ));
 });
 
 app.listen(3000);
--- week1/routes/shop.js
+++ week1/routes/shop.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const rootDir = require('../util/path');
+
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop', path:'/', hasProducts: products.length > 0});
+    //console.log(adminData.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
