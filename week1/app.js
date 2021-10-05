dad0294cc6891d833f0fcd0137b4591048fd573a
Mark Baldwin <baldwin.mv05@gmail.com>
2021-10-04 19:46:50 -0600

    Up and running with controllers finally!

--- week1/app.js
+++ week1/app.js
@@ -1,11 +1,11 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
-const adminData = require('./routes/admin.js');
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
-const exp = require('constants');
 
 const app = express();
 
@@ -16,13 +16,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404pageNotFound', {docTitle: '404 Page not Found'})
-    //res.status(404).sendFile(path.join(__dirname, 'Views', '404pageNotFound.html' ));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
 
